a94166e12dba35e8ed92e4ca01bab1228a0a4d4b
AlexanderBorisovVrn <sanchosdisaster@mail.ru>
2022-06-07 18:08:21 +0300

    feat:add pagination

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import style from "./CardList.module.scss";
+import Pagination from "@mui/material/Pagination";
 import { IProduct } from "../../types/types";
 import CardItem from "../CardItem/CardItem";
 
-const { Cards_List, Sort, Wrap } = style;
+const { Cards_List, Sort, Wrap, _Pagination } = style;
+
 type Props = {
   children: IProduct[];
   setFilter: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const CardList = ({ children, setFilter }: Props) => {
+  const [currentPage,setCurrentPage]=useState(1);
+
+  const filterHandler = (e: React.ChangeEvent<HTMLSelectElement>) =>
+    setFilter(e.target.value);
+  const paginationHandler = (event: React.ChangeEvent<unknown>, page: number) =>{setCurrentPage(page)}
+  const itemsInPage:number = 6;
+  const pagesCount = Math.ceil(children.length/itemsInPage);
+  const start = (currentPage-1)*itemsInPage;
+  const end = currentPage*itemsInPage;
+
   const dataView = children.map((item, idx) => {
     return <CardItem item={item} key={idx} />;
-  });
+  }).slice(start,end);
 
   return (
     <section className={Wrap}>
@@ -21,16 +33,21 @@ const CardList = ({ children, setFilter }: Props) => {
         <select
           name="sort"
           id="sort"
-          defaultValue={'-'}
-          onChange={(e) => setFilter(e.target.value)}
+          defaultValue={"-"}
+          onChange={filterHandler}
         >
-          <option value="-" disabled>-</option>
+          <option value="-" disabled>
+            -
+          </option>
 
           <option value="priceUp">Сначала дешевые</option>
           <option value="priceDown">Сначала дорогие</option>
         </select>
       </div>
       <ul className={Cards_List}>{dataView}</ul>
+      <div className={_Pagination}>
+        <Pagination count={pagesCount} color="primary" page={currentPage} onChange={paginationHandler} />
+      </div>
     </section>
   );
 };
